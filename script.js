7356d3203a9116fe5d62b6f88d2f6c8f9b9a07a7
jeniferrayane <jeniferlima612@gmail.com>
2024-07-17 17:31:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo simples de script JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Página carregada e script JavaScript funcionando.");
+    // Aqui você pode adicionar funcionalidades extras, como o quiz
+});
